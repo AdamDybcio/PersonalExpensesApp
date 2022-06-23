6238da5399e6ae916fc8e9da4865c82f1617986a
FuzionOrigins <nieznam2016@gmail.com>
2022-06-23 02:27:23 +0200

    Personal Expenses App 1.0.0

---
+++ lib/widgets/adaptive_flat_button.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveButton extends StatelessWidget {
+  final String text;
+  final VoidCallback handler;
+
+  AdaptiveButton(this.text, this.handler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(
+              text,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: handler,
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            onPressed: handler,
+            child: Text(
+              text,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          );
+  }
+}
---
+++ lib/widgets/chart.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'package:intl/intl.dart';
+import 'package:personal_expenses/widgets/chart_bar.dart';
+
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionsValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      print(DateFormat.E().format(weekDay));
+      print(totalSum);
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum,
+      };
+    }).reversed.toList();
+  }
+
+  double get totalSpending {
+    return groupedTransactionsValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(groupedTransactionsValues);
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionsValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'].toString(),
+                (data['amount'] as double),
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
